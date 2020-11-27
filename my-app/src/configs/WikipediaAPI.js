e6ba90ed4845ff0983b9f81107881c0f54d306f8
zenraiching <rickdavidsangalang@gmail.com>
2020-11-28 01:34:23 +0800

    removed commented code

--- my-app/src/configs/WikipediaAPI.js
+++ my-app/src/configs/WikipediaAPI.js
@@ -18,27 +18,3 @@ export const  PlusSign = ()=>{
     
 }
 
-
-
-
-
-// import React, {useState,useEffect} from 'react';
-// function PlusSign() {
-//     useEffect(()=>{
-//         fetchItem();
-        
-//     },[]);
-//     const [item,setItem]= useState({});
-//     const fetchItem= async()=>{
-//         const fetchItem=await fetch(
-//             `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow`
-//         );
-
-//         const item = await fetchItem.json();
-//         setItem(item)
-//         console.log(item)
-
-//     }
-    
-// }
-// export default PlusSign
