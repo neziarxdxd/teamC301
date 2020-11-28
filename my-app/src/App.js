129ae635e58887db36eb804a40bcbfa48c7b0903
zenraiching <rickdavidsangalang@gmail.com>
2020-11-29 01:02:22 +0800

    add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Chatbot from 'react-chatbot-kit'
 import './App.css';
-
 import ActionProvider from './configs/ActionProvider';
 import MessageParser from './configs/MessageParser';
 import config from './configs/config';
@@ -9,9 +8,9 @@ import config from './configs/config';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+    
         <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser} />
-      </header>
+      
     </div>
   );
 }
